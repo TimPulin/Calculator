e72e63f78fd7bb43697115e7c0c06d2d34fb9af6
TimPulin <kyliki@yandex.ru>
2022-07-20 18:59:17 +0300

    Update GalleryControl.js

--- GalleryControl.js
+++ GalleryControl.js
@@ -5,45 +5,74 @@ $(document).ready(function() {
 
     // изменение цвета кнопок прокрутки
     $(document).ready(function() {
-        $('.JS_Gallery-Control').on('touchstart mousedown', function() {
+        $('.JS_Gallery-ControlButton').on('touchstart mousedown', function() {
             $(this).css('background-color', colorActve);
         })
-        $('.JS_Gallery-Control').on('touchend mouseup', function() {
+        $('.JS_Gallery-ControlButton').on('touchend mouseup', function() {
             $(this).css('background-color', colorRegular);
         })
     })    // END изменение цвета кнопок прокрутки
 
     $(document).ready(function() {
-        let arrPositionsOfTabs = {};
+        const JQ_ITEMS = $('.JS_Gallery-Item'),
+              V_ITEMS = document.querySelectorAll('.JS_Gallery-Item');
+        let arrCoordsCurrent = [],
+            V_Step = item => item.offsetWidth,
+            Round = digits => parseFloat(digits.toFixed(2) ),
+            stepForward,
+            coordForStepAround,
+            indexFirstVisibleItem,
+            rating;
 
-        $('.JS_Gallery-Control').click(function() {
-            const WindowPositionLeft = 1 + $('.JS_Gallery-Window').offset().left;
-            const WindowWidth = $('.JS_Gallery-Window').css('width');
-            const WindowPostitonRight = WindowPositionLeft + WindowWidth ;
-            let widthOfFirstVisibleTab,
-                tabPositionLeft,
-                tabPositionTop,
-                galleryTabs = $('.JS_Gallery-Tab'),
-                indexOfFirstVisibleTab;
-
-
-            $('.JS_Gallery-Tab').each(function(index) {
-console.log(WindowPositionLeft +'  '+ WindowWidth)
-                tabPositionLeft = $(this).offset().left;
-                tabPositionTop = $(this).offset().top;
-                tabPositionLeft -= 300;
-                $(this).offset({top:tabPositionTop, left:tabPositionLeft})
-            })
-
-
+        $('.JS_Gallery-ControlButton').click(function() {
+            getCoordsCurrent();
+            stepForward = V_Step( V_ITEMS[getMaxMinValue(arrCoordsCurrent).indexMin] );
+            coordForStepAround = arrCoordsCurrent[getMaxMinValue(arrCoordsCurrent).indexMax] - stepForward + V_Step( V_ITEMS[getMaxMinValue(arrCoordsCurrent).indexMax]);
+            moveLeft();
         })
 
-        function getPositionsOfTabs() {
-            $('.JS_Gallery-Window').find('.JS_Gallery-Tab').offset(function(index, coords) {
-
+        function getCoordsCurrent() {
+            JQ_ITEMS.each(function(index) {
+                arrCoordsCurrent.push( Round( $(this).offset().left) );
             })
         }
-    })
+
+        function moveLeft() {
+            JQ_ITEMS.each(function(index) {
+                if(index == getMaxMinValue(arrCoordsCurrent).indexMin) {
+                    $(this).offset({left: coordForStepAround});
+                } else {
+                    $(this).offset({left:arrCoordsCurrent[index] - stepForward});
+                }
+            })
+            reset_arrCoordsCurrent();
+        }
+
+        function reset_arrCoordsCurrent() {
+            arrCoordsCurrent.splice(0, arrCoordsCurrent.length);
+        }
+
+        function getMaxMinValue(arr) {
+            let rating = {
+                max: arr[0],
+                min: arr[0],
+                indexMax: 0,
+                indexMin: 0
+            }
+
+            for(let i = 0; i < arr.length; ++i) {
+                if(arr[i] > rating.max) {
+                    rating.max = arr[i];
+                    rating.indexMax = i;
+                } else if(arr[i] < rating.min) {
+                    rating.min = arr[i];
+                    rating.indexMin = i
+                }
+            }
+            return rating;
+        } //END getMaxMinValue
+
+    }) //END прокрутка закладок
 
 })
 // END GalleryControl.js
